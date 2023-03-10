c0b47dfe5c42dbc300263a5052016abba9746e23
Suleyman <suleymanali76@gmail.com>
2023-03-10 03:49:12 +0300

    Refactoring #4

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,21 @@
 module.exports = {
   content: ['./**/*.{html,js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      xs: '576px',
+      sm: '640px',
+      md: '768px',
+      lg: '992px',
+      xl: '1280px',
+      '2xl': '1500px',
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1.25rem',
+        sm: '0',
+      },
+    },
     extend: {},
   },
   plugins: [],
